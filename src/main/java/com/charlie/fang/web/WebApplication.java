6f85f12fc9870ed15514d4c8f2be1cff98e43207
charlie <jwjw233233@163.com>
2019-09-28 16:00:04 +0800

    feat:init

--- src/main/java/com/charlie/fang/web/WebApplication.java
+++ src/main/java/com/charlie/fang/web/WebApplication.java
@@ -1,11 +1,17 @@
 package com.charlie.fang.web;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@SpringBootApplication
+@Controller
 public class WebApplication {
 
     @RequestMapping("/")
+    @ResponseBody
     String home() {
         return "Hello World!";
     }
